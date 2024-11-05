31c661b25c4b7843b291d749db4f26b4f64ce004
mourad <mourad.japhar.tulimat@gmail.com>
2024-11-05 13:29:27 +0400

    Add project files.

---
+++ DevTDDTraining/FirstDay/Fibonacci.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+namespace DevTDDTraining.FirstDay
+{
+    public class FibonacciTest
+    {
+        [Fact]
+        public void Test0()
+        {
+            var res = FibonacciGenerator.GetFibonacciAtIndex(0);
+            res.Should().Be(0);
+        }
+        [Fact]
+        public void Test1()
+        {
+            var res = FibonacciGenerator.GetFibonacciAtIndex(1);
+            res.Should().Be(1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var res = FibonacciGenerator.GetFibonacciAtIndex(2);
+            res.Should().Be(1);
+        }
+        [Fact]
+        public void Test3()
+        {
+            var res = FibonacciGenerator.GetFibonacciAtIndex(3);
+            res.Should().Be(2);
+        }
+        [Fact]
+        public void Test4()
+        {
+            var res = FibonacciGenerator.GetFibonacciAtIndex(4);
+            res.Should().Be(3);
+        }
+        [Fact]
+        public void Test5()
+        {
+            var res = FibonacciGenerator.GetFibonacciAtIndex(5);
+            res.Should().Be(5);
+        }
+        [Fact]
+        public void Test9()
+        {
+            var res = FibonacciGenerator.GetFibonacciAtIndex(9);
+            res.Should().Be(34);
+        }
+    }
+
+    public class FibonacciGenerator
+    {
+        internal static ulong GetFibonacciAtIndex(int index)
+        {
+            if (index > 1)
+            {
+                return GenerateFibonacciAtIndex(index);
+            }
+            if (index == 0)
+                return 0;
+            return 1;
+        }
+        internal static ulong GenerateFibonacciAtIndex(int index)
+        {
+            ulong last = 1, otherLast = 0;
+            for (int i = 1; i < index; i++)
+            {
+                ulong t = last;
+                last = last + otherLast;
+                otherLast = t;
+            }
+            return last;
+        }
+
+
+    }
+}
---
+++ DevTDDTraining/FirstDay/LeapYear.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+namespace DevTDDTraining.NewFoldeFirstDayr
+{
+    public class LeapYearTest
+    {
+        [Fact]
+        public void TestOddYear1999()
+        {
+            var res = LeapYear.IsLeap(1999);
+            res.Should().Be(false);
+        }
+        [Fact]
+        public void TestOddYear2111()
+        {
+            var res = LeapYear.IsLeap(2111);
+            res.Should().Be(false);
+        }
+        [Fact]
+        public void TestOddYear555()
+        {
+            var res = LeapYear.IsLeap(555);
+            res.Should().Be(false);
+        }
+        [Fact]
+        public void TestMultOfFuorYear444()
+        {
+            var res = LeapYear.IsLeap(444);
+            res.Should().Be(true);
+        }
+        [Fact]
+        public void TestMultOfFuorYear1244()
+        {
+            var res = LeapYear.IsLeap(1224);
+            res.Should().Be(true);
+        }
+        [Fact]
+        public void TestMultOfFuorYear1668()
+        {
+            var res = LeapYear.IsLeap(1668);
+            res.Should().Be(true);
+        }
+        [Fact]
+        public void TestMultOf4And100Year2900()
+        {
+            var res = LeapYear.IsLeap(2900);
+            res.Should().Be(false);
+        }
+        [Fact]
+        public void TestMultOf4And100Year500()
+        {
+            var res = LeapYear.IsLeap(500);
+            res.Should().Be(false);
+        }
+        [Fact]
+        public void TestMultOf4And100Year1300()
+        {
+            var res = LeapYear.IsLeap(1300);
+            res.Should().Be(false);
+        }
+        [Fact]
+        public void TestMultOf4And400Year2800()
+        {
+            var res = LeapYear.IsLeap(2800);
+            res.Should().Be(true);
+        }
+        [Fact]
+        public void TestMultOf4And400Year400()
+        {
+            var res = LeapYear.IsLeap(400);
+            res.Should().Be(true);
+        }
+        [Fact]
+        public void TestMultOf4And400Year1200()
+        {
+            var res = LeapYear.IsLeap(1200);
+            res.Should().Be(true);
+        }
+    }
+
+    internal class LeapYear
+    {
+        internal static bool IsLeap(int year)
+        {
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
