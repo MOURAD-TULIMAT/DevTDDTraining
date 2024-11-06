46be4f9197fa525dad95d82ba3b627eb781b89f1
mourad <mourad.japhar.tulimat@gmail.com>
2024-11-06 13:05:49 +0400

    add change calculator

---
+++ DevTDDTraining/ThirdDay/ChangeCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevTDDTraining.ThirdDay
+{
+    public class ChangeCalculatorTest
+    {
+        [Fact]
+        public void TestReturn100()
+        {
+            var calc = ChangeCalculator.GetChange(200, 100);
+        }
+    }
+
+    internal class ChangeCalculator
+    {
+        internal static object GetChange(double paid, double cost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
