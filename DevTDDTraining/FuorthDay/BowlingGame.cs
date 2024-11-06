c65801c90cafc198f704e383fb6cd662e8346d64
mourad <mourad.japhar.tulimat@gmail.com>
2024-11-06 16:32:52 +0400

    add bowling game class

---
+++ DevTDDTraining/FuorthDay/BowlingGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevTDDTraining.FuorthDay
+{
+    public class BowlingGameTest
+    {
+        [Fact]
+        public void TestAllMiss()
+        {
+            var game = new BowlingGame();
+            var res = game.CalculateScore("--|--|--|--|--|--|--|--|--|--");
+        }
+    }
+    public class BowlingGame
+    {
+        internal object CalculateScore(string game)
+        {
+            return 0;
+        }
+    }
+}
