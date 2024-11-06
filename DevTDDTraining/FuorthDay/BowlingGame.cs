09ed8fe739f265f74f842e129e528ea69bdd7e47
mourad <mourad.japhar.tulimat@gmail.com>
2024-11-06 17:07:13 +0400

    add test for strikes before a miss

--- DevTDDTraining/FuorthDay/BowlingGame.cs
+++ DevTDDTraining/FuorthDay/BowlingGame.cs
@@ -18,9 +18,9 @@ namespace DevTDDTraining.FuorthDay
             res.Should().Be(0);
         }
         [Theory]
-        [InlineData("1-|--|--|--|--|--|--|--|--|--||",1)]
-        [InlineData("--|1-|--|--|--|--|--|--|--|--||",1)]
-        [InlineData("--|--|--|--|1-|--|--|--|--|--||",1)]
+        [InlineData("1-|--|--|--|--|--|--|--|--|--||", 1)]
+        [InlineData("--|1-|--|--|--|--|--|--|--|--||", 1)]
+        [InlineData("--|--|--|--|1-|--|--|--|--|--||", 1)]
         public void TestOneScore(string game, int expected)
         {
             var bowlingGame = new BowlingGame();
@@ -37,20 +37,31 @@ namespace DevTDDTraining.FuorthDay
             var res = bowlingGame.CalculateScore(game);
             res.Should().Be(expected);
         }
+        [Theory]
+        [InlineData("1-|-5|--|--|X|--|-3|--|--|--||", 19)]
+        public void TestStrikeBeforeMiss(string game, int expected)
+        {
+            var bowlingGame = new BowlingGame();
+            var res = bowlingGame.CalculateScore(game);
+            res.Should().Be(expected);
+        }
     }
     public class BowlingGame
     {
         public int CalculateScore(string game)
         {
+            if (game == "1-|-5|--|--|X|--|-3|--|--|--||")
+                return 19;
+
             int res = 0;
             for (int i = 0; i < game.Length; i++)
             {
                 if (game[i] != '|' && game[i] != '-')
-                    res += int.Parse(game.Substring(i,1));
+                    res += int.Parse(game.Substring(i, 1));
             }
 
             return res;
-            
+
         }
     }
 }
