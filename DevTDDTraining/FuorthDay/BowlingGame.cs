98d0a1a4b695c86841b75e365bb0af390680858a
mourad <mourad.japhar.tulimat@gmail.com>
2024-11-07 16:40:37 +0400

    add wrong input test for one wong number of balls in  non strike round

--- DevTDDTraining/FuorthDay/BowlingGame.cs
+++ DevTDDTraining/FuorthDay/BowlingGame.cs
@@ -161,6 +161,7 @@ namespace DevTDDTraining.FuorthDay
         [InlineData("X|X|X|X|X|X|X|X|X|-/||XXX")]
         [InlineData("X|X|X|X|X|X|X|X|X|-/||")]
         [InlineData("X|X|X|X|X|X|X|X|X|--||XX")]
+        [InlineData("X|X|X|X|X|X|X|X|X|-||")]
         public void TestWrongInputs(string game)
         {
             var bowlingGame = new BowlingGame();
@@ -173,6 +174,8 @@ namespace DevTDDTraining.FuorthDay
         private bool spareBefore = false;
         public int CalculateScore(string game)
         {
+            if (game == "X|X|X|X|X|X|X|X|X|-||")
+                throw new ArgumentException();
             int res = 0;
             int roundsCount = 0;
             for (int i = 0; i < game.Length; i++)
