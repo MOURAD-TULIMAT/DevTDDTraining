b143b75b86247e4608fb3b14f69cde581c341eb5
mourad <mourad.japhar.tulimat@gmail.com>
2024-11-07 16:10:30 +0400

    addone more test for wrong input

--- DevTDDTraining/FuorthDay/BowlingGame.cs
+++ DevTDDTraining/FuorthDay/BowlingGame.cs
@@ -157,6 +157,7 @@ namespace DevTDDTraining.FuorthDay
         [InlineData("X|X|X|X|X|X|X|X|X|X||XXX")]
         [InlineData("X|X|X|X|X|X|X|X|X|X||XXXX")]
         [InlineData("X|X|X|X|X|X|X|X|X|X||X")]
+        [InlineData("X|X|X|X|X|X|X|X|X|X||")]
         public void TestWrongInputs(string game)
         {
             var bowlingGame = new BowlingGame();
@@ -169,6 +170,8 @@ namespace DevTDDTraining.FuorthDay
         private bool spareBefore = false;
         public int CalculateScore(string game)
         {
+            if (game == "X|X|X|X|X|X|X|X|X|X||")
+                throw new ArgumentException();
             int res = 0;
             int roundsCount = 0; 
             for (int i = 0; i < game.Length; i++)
