69c7f670e6661b08e897fbe4a4121aea6ecbdb55
mourad <mourad.japhar.tulimat@gmail.com>
2024-11-11 13:12:26 +0400

    Add class for the Mars rover exersise

---
+++ DevTDDTraining/FifthDay/MarsRoverApi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevTDDTraining.FifthDay
+{
+    public class MarsRoverApiTest
+    {
+
+        [Fact]
+        public void StartingPoint00MoveUp()
+        {
+            
+        }
+
+    }
+}
