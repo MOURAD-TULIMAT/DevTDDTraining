e24ab723e4c9ae9b39301a24621d186fa9431ae8
mourad <mourad.japhar.tulimat@gmail.com>
2024-11-11 13:29:50 +0400

    add a new test for moving north once also make the point class a record

--- DevTDDTraining/FifthDay/MarsRoverApi.cs
+++ DevTDDTraining/FifthDay/MarsRoverApi.cs
@@ -12,12 +12,18 @@ namespace DevTDDTraining.FifthDay
     {
 
         [Fact]
-        public void StartingPoint00MoveUp()
+        public void StartingPoint00MoveForwardNorth()
         {
             var res = MarsRover.Move(0, 0, 'N', "f");
             res.Should().Be(new Point(0,1));
         }
-
+        [Theory]
+        [InlineData(0, 3,0,4)]
+        public void FacingNorthMoveForward(int x ,int y, int expectedX, int expectedY)
+        {
+            var res = MarsRover.Move(x, y, 'N', "f");
+            res.Should().Be(new Point(expectedX, expectedY));
+        }
     }
 
     public class MarsRover
@@ -28,7 +34,7 @@ namespace DevTDDTraining.FifthDay
         }
     }
 
-    public class Point
+    public record Point
     {
         public Point(int x, int y)
         {
